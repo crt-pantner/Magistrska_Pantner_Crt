6f85400413a513f8f18d9de4c78b11939ffe623b
crt-pantner <crt.pantner@protonmail.com>
2026-07-20 20:07:19 +0200

    Added automatic output handling into specified folder via CLI, added handling of dummy proteins.

--- scripts/hmmer_features/features.py
+++ scripts/hmmer_features/features.py
@@ -25,146 +25,185 @@ def get_arguments():
     args = parser.parse_args()
     return args
 
+
+def open_json(path: Path):
+    json_dict = {}
+
+    with open(path, "r") as json_file:
+        json_dict = json.load(json_file)
+
+
+    json_dict["result"]["stats"]["id"] = path.stem.rstrip("_hmmerrez")
+    
+
+    return json_dict
+
+# Handling CLI
 args = get_arguments()
-file_path = args.dir
+json_folder = args.dir
 
-with open(file_path, "r") as json_file:
-    data = json.load(json_file)
+# Handle creating results directory if necessary
+Path(args.output).mkdir(parents=True, exist_ok=True)
+outdir = Path(args.output)
 
+# Fetch all json files.
+json_paths: list[Path] = []
+for path in Path(json_folder).rglob("*.json"):
+    json_paths.append(path)
 
-if data["status"] != "SUCCESS":
-    raise ValueError
+for json_file in json_paths:
+    print(json_file.stem)
 
-results = data["result"]
+    data = open_json(json_file)               
+    hits = data["result"]["hits"]
 
-stats = results["stats"]
+    # Check that the result is valid
+    if data["status"] not in ["SUCCESS", "dummy"]:
+        raise ValueError
 
-hits = results["hits"]
+    results = data["result"]
+    stats = results["stats"]
+    hits = results["hits"]
 
-factual_hits = []
+    factual_hits = []
 
-for hit in hits:
-    if hit["is_included"] and hit["is_reported"]:
-        factual_hits.append(hit)
-
-
-if not factual_hits:
-    raise ValueError
-
-
-lenght_of_protein_sequence = data["result"]["hits"][0]["domains"][0][
-    "alignment_display"
-]["l"]
-scaled_len_protein = lenght_of_protein_sequence / 2
-size_of_svg = scaled_len_protein + 2
-
-
-svg = ET.Element(
-    "svg",
-    width=f"{size_of_svg}",
-    height="20",
-    viewBox=f"0 0 {size_of_svg} 20",
-    style=f"width: {size_of_svg * 2}px; height: 40px",
-    xmlns="http://www.w3.org/2000/svg",
-)
-
-defs = ET.SubElement(svg, "defs")
-
-seq_rect = ET.SubElement(svg, "g", transform="translate(0 10)")
-
-ET.SubElement(
-    seq_rect,
-    "rect",
-    x="0",
-    y="-2.5",
-    width=f"{scaled_len_protein}",
-    height="5",
-    fill="#d8d8d8",
-    rx="2.5",
-    ry="2.5",
-)
-
-factual_domains = []
-
-jagged_end_right = "h5l-2.5,2.5l2.5,2.5l-2.5,2.5l2.5,2.5h-5"
-jagged_end_left = "h-5l2.5,-2.5l-2.5,-2.5l2.5,-2.5l-2.5,-2.5h5"
-
-straight_end_left = "5v10h-5h"
-straight_end_right = "-5v-10h5z"
-
-round_end_left = "A5,5,0,0,1,130.5,10h"  # 130.5 je samo širina domene + 5
-round_end_right = "A5,5,0,0,1,5,0"
+    for hit in hits:
+        if hit["is_included"] and hit["is_reported"]:
+            factual_hits.append(hit)
 
 
-for hit in factual_hits:
-    for i, domain in enumerate(hit["domains"]):
-        if domain["is_reported"] and domain["is_included"]:
-            factual_domains.append(domain)
+    if not factual_hits:
+        raise ValueError
 
-            ienv = domain["ienv"] / 2
-            iali = domain["iali"] / 2
-            jali = domain["jali"] / 2
-            jenv = domain["jenv"] / 2
 
-            mask = ET.SubElement(
-                defs,
-                "mask",
-                id=f"{domain['alignment_display']['hmmacc']}_{i}",
-                width=f"{jenv - ienv}",
-                height="10",
-                fill="#fff",
-            )
+    lenght_of_protein_sequence = data["result"]["hits"][0]["domains"][0][
+        "alignment_display"
+    ]["l"]
+    scaled_len_protein = lenght_of_protein_sequence / 2
+    size_of_svg = scaled_len_protein + 2
 
-            for x, width, opacity in [
-                (0, iali - ienv, 0.6),
-                (iali - ienv, jali - iali, 1),
-                (jali - ienv, jenv - jali, 0.6),
-            ]:
-                ET.SubElement(
-                    mask,
-                    "rect",
-                    x=str(x),
-                    y="0",
-                    width=str(width),
+
+    svg = ET.Element(
+        "svg",
+        width=f"{size_of_svg}",
+        height="20",
+        viewBox=f"0 0 {size_of_svg} 20",
+        style=f"width: {size_of_svg * 2}px; height: 40px",
+        xmlns="http://www.w3.org/2000/svg",
+    )
+
+    defs = ET.SubElement(svg, "defs")
+
+    seq_rect = ET.SubElement(svg, "g", transform="translate(0 10)")
+
+    ET.SubElement(
+        seq_rect,
+        "rect",
+        x="0",
+        y="-2.5",
+        width=f"{scaled_len_protein}",
+        height="5",
+        fill="#d8d8d8",
+        stroke="#00000050",
+        stroke_width="0.001",
+        st="1%",
+        rx="2.5",
+        ry="2.5",
+    )
+
+    factual_domains = []
+
+    jagged_end_right = "h5l-2.5,2.5l2.5,2.5l-2.5,2.5l2.5,2.5h-5"
+    jagged_end_left = "h-5l2.5,-2.5l-2.5,-2.5l2.5,-2.5l-2.5,-2.5h5"
+
+    straight_end_left = "5v10h-5h"
+    straight_end_right = "-5v-10h5z"
+
+    round_end_left = "A5,5,0,0,1,130.5,10h"  # 130.5 je samo širina domene + 5
+    round_end_right = "A5,5,0,0,1,5,0"
+
+
+    for hit in factual_hits:
+        for i, domain in enumerate(hit["domains"]):
+            if domain["is_reported"] and domain["is_included"]:
+                factual_domains.append(domain)
+
+                ienv = domain["ienv"] / 2
+                iali = domain["iali"] / 2
+                jali = domain["jali"] / 2
+                jenv = domain["jenv"] / 2
+
+                mask = ET.SubElement(
+                    defs,
+                    "mask",
+                    id=f"{domain['alignment_display']['hmmacc']}_{i}",
+                    width=f"{jenv - ienv}",
                     height="10",
-                    opacity=str(opacity),
+                    fill="#fff",
                 )
 
-            domain_len = ienv
-            g = ET.SubElement(
-                svg,
-                "g",
-                transform=f"translate({domain_len}, 5)",
-                dataentity=domain["alignment_display"]["hmmacc"],
-            )
+                for x, width, opacity in [
+                    (0, iali - ienv, 0.6),
+                    (iali - ienv, jali - iali, 1),
+                    (jali - ienv, jenv - jali, 0.6),
+                ]:
+                    ET.SubElement(
+                        mask,
+                        "rect",
+                        x=str(x),
+                        y="0",
+                        width=str(width),
+                        height="10",
+                        opacity=str(opacity),
+                    )
 
-            path = ET.SubElement(
-                g,
-                "path",
-                d=f"m5,0h{(jenv - ienv) - 10}h{straight_end_left}{-((jenv - ienv) - 10)}h{straight_end_right}",
-                fill=hit["metadata"]["color"],
-                mask=f"url(#{domain['alignment_display']['hmmacc']}_{i})",
-            )
-            text_attributes = {
-                "x": f"{(jenv - ienv) / 2}",
-                "y": "8",
-                "text-anchor": "middle",
-                "font-size": "7.5",
-                "fill": "white",
-                "opacity": "1",
-                "data-maxwidth": "61",
-                "font-family": "Monospace",
-            }
-            domain_name = hit["metadata"]["identifier"]
+                domain_len = ienv
+                g = ET.SubElement(
+                    svg,
+                    "g",
+                    transform=f"translate({domain_len}, 5)",
+                    dataentity=domain["alignment_display"]["hmmacc"],
+                )
 
-            text_element = ET.SubElement(g, "text", attrib=text_attributes)
-            text_element.text = domain_name
+                path = ET.SubElement(
+                    g,
+                    "path",
+                    d=f"m5,0h{(jenv - ienv) - 10}h{straight_end_left}{-((jenv - ienv) - 10)}h{straight_end_right}",
+                    fill=hit["metadata"]["color"],
+                    mask=f"url(#{domain['alignment_display']['hmmacc']}_{i})",
+                )
+                text_attributes = {
+                    "x": f"{(jenv - ienv) / 2}",
+                    "y": "8",
+                    "text-anchor": "middle",
+                    "font-size": "7.5",
+                    "fill": "white",
+                    "opacity": "1",
+                    "data-maxwidth": "61",
+                    "font-family": "Monospace",
+                }
+                
+                domain_name = hit["metadata"]["identifier"]
+                
+
+                text_element = ET.SubElement(g, "text", attrib=text_attributes)
+                
+                if domain_name == "dummmy":
+                    text_element.text = ""
+                else:
+                    text_element.text = domain_name
+                
+                
 
 
-print(len(factual_domains))
+    print(len(factual_domains))
 
 
-ET.indent(svg, space="    ", level=0)
-tree = ET.ElementTree(svg)
+    ET.indent(svg, space="    ", level=0)
+    tree = ET.ElementTree(svg)
 
-tree.write(args.output, encoding="utf-8", xml_declaration=False)
+
+    outpath = outdir / Path(json_file.stem).with_suffix(".svg")
+
+
+    tree.write(outpath, encoding="utf-8", xml_declaration=False)
