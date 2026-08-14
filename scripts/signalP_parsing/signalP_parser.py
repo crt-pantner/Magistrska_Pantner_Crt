e1fc038af564f661e80648e85c33edd1739c7dc0
crt-pantner <crt.pantner@protonmail.com>
2026-08-14 21:07:38 +0200

    Improved output, results are now output in two graphs - probability of a seqhuence having a signal sequence vs "other".

--- scripts/signalP_parsing/signalP_parser.py
+++ scripts/signalP_parsing/signalP_parser.py
@@ -3,15 +3,27 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+
 with open(input("json summary file: "), "r") as file:
     data = json.load(file)
 
+non_signal_sequence = []
 parsed_data = []
 
 for sequence in data["SEQUENCES"]:
-    value = data["SEQUENCES"][sequence]["Likelihood"][0] #ker imamo zbrano na koncu nič, se nam izpišejo verjetnosti, da je ta protein signalna molekula oz. signalni peptid.
-    pair = [sequence, value]
-    parsed_data.append(pair)
+    if data["SEQUENCES"][sequence]["Prediction"] == "Other":
+        value = data["SEQUENCES"][sequence]["Likelihood"][0] #ker imamo zbrano na koncu nič, se nam izpišejo verjetnosti, da je ta protein signalna molekula oz. signalni peptid.
+        pair = [sequence, value]
+        non_signal_sequence.append(pair)
+    else:
+        value = data["SEQUENCES"][sequence]["Likelihood"][1]
+        value = float(value) #ker imamo zbrano na koncu nič, se nam izpišejo verjetnosti, da je ta protein signalna molekula oz. signalni peptid.
+        pair = [sequence, value]
+        parsed_data.append(pair)
+
+
+print(non_signal_sequence)
+    
 
 
 
@@ -32,3 +44,17 @@ g.set_ylabel("Count")
 
 
 plt.savefig("histogram.png")
+
+non_signal_sequence = pd.DataFrame(data=non_signal_sequence, columns=names)
+
+filtered_df = non_signal_sequence[non_signal_sequence["probability"] >= 0.05]
+
+sns.set_theme(style="darkgrid")
+sns.set_style("ticks")
+sns.set_palette("viridis")
+g = sns.histplot(data=non_signal_sequence)
+g.set_xlabel("Probability of sequence being 'OTHER'")
+g.set_ylabel("Count")
+
+
+plt.savefig("histogram_no_signal.png")
